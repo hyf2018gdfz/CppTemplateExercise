02ea13fae97e93f04b9a4192e1157c5bad91ed14
Fmyh1828 <3256411401@qq.com>
2026-01-13 11:50:21 +0800

    update: Multiset

---
+++ test/tMultiset.cpp
@@ -0,0 +1,64 @@
+#include <set>
+
+#include "Set.hpp"
+#include "test.h"
+#include "testcase.h"
+using mystd::set::Multiset;
+using std::multiset;
+
+void test_Multiset() {
+  RandomGenerator gen;
+  const int query_times = 1000000;
+  const int num_range = 100;
+  Multiset<int> st;
+  multiset<int> ref;
+  for (int t = 0; t < query_times; t++) {
+    int opt = gen.uniform_int(0, 5);
+    int rndint = gen.uniform_int(0, num_range);
+    switch (opt) {
+      case 0: {
+        CHECK_EQ(ref.empty(), st.empty());
+        break;
+      }
+      case 1: {
+        CHECK_EQ(ref.size(), st.size());
+        break;
+      }
+      case 2: {
+        auto my_ans = st.insert(rndint);
+        auto ref_ans = ref.insert(rndint);
+        CHECK_EQ(*ref_ans, *my_ans);
+        break;
+      }
+      case 3: {
+        auto my_ans = st.erase(rndint);
+        auto ref_ans = ref.erase(rndint);
+        CHECK_EQ(ref_ans, my_ans);
+        break;
+      }
+      case 4: {
+        auto my_ans = (st.find(rndint) == st.end());
+        auto ref_ans = (ref.find(rndint) == ref.end());
+        CHECK_EQ(ref_ans, my_ans);
+        break;
+      }
+      case 5: {
+        auto my_ans = st.equalRange(rndint);
+        auto ref_ans = ref.equal_range(rndint);
+        if (ref_ans.first == ref.end()) {
+          CHECK_EQ(true, (my_ans.first == st.end()));
+        } else {
+          CHECK_EQ((*ref_ans.first), (*my_ans.first));
+        }
+        if (ref_ans.second == ref.end()) {
+          CHECK_EQ(true, (my_ans.second == st.end()));
+        } else {
+          CHECK_EQ((*ref_ans.second), (*my_ans.second));
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  }
+}
